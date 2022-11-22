0bcdda349c9320d13fa6790a23038ccf50b0b8d7
Yakumo Saki <yakumo@ziomatrix.org>
2022-11-23 01:05:02 +0900

    exec build.sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+# local docker image tag
+DOCKER_TAG=`date '+%Y%m%d_%H'`
+DOCKERHUB_IMAGENAME="yakumosaki/glitch-soc-aarch64"
+
+TOOT=/work/jenkins-scripts/foundation_admin_toots.sh
+WORK=/work/docker-build/docker-glitch-soc
+
+# PULL
+cd ${WORK}
+git pull
+
+cd ${WORK}/mastodon
+git reset --hard
+git pull
+
+${WORK}/prepare.sh $DOCKERHUB_IMAGENAME $DOCKER_TAG
+
+# BUILD
+ARCH=`uname -m`
+cd $WORK
+echo "building docker image ${DOCKERHUB_IMAGENAME}:${DOCKER_TAG} on `pwd`"
+
+docker buildx build -t $DOCKERHUB_IMAGENAME:$DOCKER_TAG -f $ARCH/Dockerfile .
+docker tag $DOCKERHUB_IMAGENAME:$DOCKER_TAG $DOCKERHUB_IMAGENAME:latest
+
+# PUSH
+docker push $DOCKERHUB_IMAGENAME:$DOCKER_TAG
+docker push $DOCKERHUB_IMAGENAME:latest
