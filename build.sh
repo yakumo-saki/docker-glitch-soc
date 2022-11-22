aae8e976cf383932a0ca9fdf218231aefa2cb0c3
Yakumo Saki <yakumo@ziomatrix.org>
2022-11-23 01:51:30 +0900

    optimized for circleci

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -eu
 
 # local docker image tag
-DOCKER_TAG=`date '+%Y%m%d_%H'`
-DOCKERHUB_IMAGENAME="yakumosaki/glitch-soc-aarch64"
 
 GIT_REPO=https://github.com/glitch-soc/mastodon.git
 
@@ -20,15 +18,11 @@ cd ${WORK}/mastodon
 git reset --hard
 git pull
 
-${WORK}/prepare.sh $DOCKERHUB_IMAGENAME $DOCKER_TAG
 
 # BUILD
-ARCH=`uname -m`
 cd $WORK
 echo "building docker image ${DOCKERHUB_IMAGENAME}:${DOCKER_TAG} on `pwd`"
 
-docker buildx build -t $DOCKERHUB_IMAGENAME:$DOCKER_TAG -f $ARCH/Dockerfile .
-docker tag $DOCKERHUB_IMAGENAME:$DOCKER_TAG $DOCKERHUB_IMAGENAME:latest
 
 # PUSH
 docker push $DOCKERHUB_IMAGENAME:$DOCKER_TAG
